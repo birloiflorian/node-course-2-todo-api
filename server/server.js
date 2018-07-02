eaebe1e8c26ae2df6f1ebd7aa8ffeab7506c96b2
Florian Birloi <birloiflorian@gmail.com>
2018-07-02 23:39:52 +0300

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ var {User} = require('./models/user');
 
 var app = express();
 
-//give this middleware to express
+//give this middleware to expressb
 app.use(bodyParser.json());
 
 //configure post route
@@ -28,43 +28,44 @@ app.post('/todos', (req, res) => {
 });
 
 //configure get request
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        //more flexible to use object
-        res.send({
-            todos
-        });
-    }, (e) => {
-        res.status(400).send(e);
-    });
-});
+// app.get('/todos', (req, res) => {
+//     Todo.find().then((todos) => {
+//         //more flexible to use object
+//         res.send({
+//             todos
+//         });
+//     }, (e) => {
+//         res.status(400).send(e);
+//     });
+// });
 
 //fetch a single todo
 //GET /todos/id
-app.get('/todos/:id', (req, res) => {
-    var id = req.params.id;
+// app.get('/todos/:id', (req, res) => {
+//     var id = req.params.id;
 
-    if(!ObjectID.isValid(id)){
-        return res.status(404).send();
-    }
+//     if(!ObjectID.isValid(id)){
+//         return res.status(404).send();
+//     }
 
-    Todo.findById(id).then((todo) => {
-        if(!todo) {
-            return res.status(404).send();
-        }
+//     Todo.findById(id).then((todo) => {
+//         if(!todo) {
+//             return res.status(404).send();
+//         }
 
-        res.send({
-            todo
-        });
-    }).catch((e) => {
-        res.status(400).send();
-    });
-});
+//         res.send({
+//             todo
+//         });
+//     }).catch((e) => {
+//         res.status(400).send();
+//     });
+// });
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
+module.exports = { app };
 
 
 
